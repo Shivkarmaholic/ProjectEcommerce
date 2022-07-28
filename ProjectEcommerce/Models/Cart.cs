14abf91648ec63bc7d9877d0af43ff31da8e00cd
Shivkarmaholic <kharadkarsandeep1@gmail.com>
2022-07-28 14:21:45 +0530

    Add project files.

---
+++ ProjectEcommerce/Models/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceProject.Models
+{
+    [Table("Cart_Table")]
+    public class Cart
+    {
+        [Key]        
+        public int c_id { get; set; }
+        [Required]
+        public string p_id { get; set; }
+        [Required]
+        public string u_id { get; set; }
+        
+        public static string ConnectionString = "Server=DESKTOP-SQKJ3UE\\SQLEXPRESS;Database=Sample;Integrated Security=True;";
+
+    }
+
+
+
+}
